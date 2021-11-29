c43716213f49a5bb3f38272d93453ccebb73c661
actionunionentertanment <94116044+actionunionentertanment@users.noreply.github.com>
2021-11-29 16:05:27 +0530

    Add files via upload

---
+++ Project-77/main.js
@@ -0,0 +1,17 @@
+var firstname="Sasuke";
+
+var lastname="Uchiha"=concat(firstname,lastname);
+
+var fullname="Ranbir Senju";
+
+var age="47";
+
+console.log( firstname);
+
+console.log(lastname);
+
+console.log( fullname);
+
+console.log( age);
+
+
